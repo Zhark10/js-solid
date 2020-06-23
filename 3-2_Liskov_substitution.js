a8f3edc24cd5997e4f8985db2e69792bd3dbd993
Zhark10 <verygoodman18@yandex.ru>
2020-06-24 01:29:23 +0300

    liskov substitution principle - second example

---
+++ 3-2_Liskov_substitution.js
@@ -0,0 +1,39 @@
+class Component {
+    isComponent = true
+}
+
+class ComponentWithTemplate extends Component {
+    render() {
+        return `<div>Component</div>`
+    }
+}
+
+class HigherOrderComponent extends Component {
+
+}
+
+class HeaderComponent extends ComponentWithTemplate {
+    onInit() {}
+}
+
+class FooterComponent extends ComponentWithTemplate {
+    afterInit() {}
+}
+
+function renderComponent(component) {
+    console.log(component.render())
+}
+
+class HOC extends HigherOrderComponent {
+    render() {
+        throw new Error('Render is impossible here')
+    }
+
+    wrapComponent(component) {
+        component.wrapped = true
+        return component
+    }
+}
+
+renderComponent(new HeaderComponent())
+renderComponent(new FooterComponent())
