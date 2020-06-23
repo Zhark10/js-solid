115ece75e62dd6b065edaa566a5e9daf52ed24b0
Zhark10 <verygoodman18@yandex.ru>
2020-06-24 01:29:04 +0300

    liskov substitution

---
+++ 3-1_Liskov_substitution.js
@@ -0,0 +1,39 @@
+class Person {
+
+}
+
+class Member extends Person {
+    access() {
+        console.log('У тебя есть доступ')
+    }
+}
+
+class Guest extends Person {
+    isGuest = true
+}
+
+class Frontend extends Member {
+    canCreateFrontend() {
+
+    }
+}
+
+class Backend extends Member {
+    canCreateBackend() {
+
+    }
+}
+
+class PersonFromDefferentCompany extends Guest {
+    access() {
+        throw new Error('У тебя нет доступа! Иди к себе!')
+    }
+}
+
+function openSecretDoor(member) {
+    member.access()
+}
+
+openSecretDoor(new Frontend())
+openSecretDoor(new Backend())
+// openSecretDoor(new PersonFromDefferentCompany()) // There should be member
